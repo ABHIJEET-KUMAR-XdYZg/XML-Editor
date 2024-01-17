443e7f2e1dd00b6c65452f0c21b7c8e48e7d2a94
ABHIJEET-KUMAR-XdYZg <133910196+ABHIJEET-KUMAR-XdYZg@users.noreply.github.com>
2024-01-17 12:39:51 +0530

    Removing

--- README.md
+++
@@ -1,2 +0,0 @@
-# KIIT_23SE03_Design_Web_Config_Xml_Editor_Using_Flutter_App
-SRIB-PRISM Program
--- xml_editor/android/app/src/main/kotlin/com/example/xml_editor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.xml_editor
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
